ce057f128a7a9dda0619d881ebb23a8f949fb091
Glorydark <61799945+glorydark@users.noreply.github.com>
2023-04-24 18:16:06 +0800

    Update Inventory.java

--- src/main/java/glorydark/customform/utils/Inventory.java
+++ src/main/java/glorydark/customform/utils/Inventory.java
@@ -2,9 +2,6 @@ package glorydark.customform.utils;
 
 import cn.nukkit.item.Item;
 import cn.nukkit.nbt.tag.CompoundTag;
-import com.sun.org.glassfish.gmbal.Description;
-
-@Description("Original Author: SmallAsWater") //引用若水的NBT物品保存代码
 
 public class Inventory {
 
