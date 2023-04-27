12a84c6c2f59d6e2060e1af3c0715971d05052a3
glorydark <1083215364@qq.com>
2023-04-27 11:05:49 +0800

    FormPreOpenEvent and FormOpenEvent

---
+++ src/main/java/glorydark/customform/event/FormPreOpenEvent.java
@@ -0,0 +1,26 @@
+package glorydark.customform.event;
+
+import cn.nukkit.Player;
+import cn.nukkit.event.player.PlayerEvent;
+import glorydark.customform.scriptForms.form.ScriptForm;
+
+public class FormPreOpenEvent extends PlayerEvent {
+
+    ScriptForm scriptForm;
+
+    Player player;
+
+    public FormPreOpenEvent(ScriptForm scriptForm, Player player){
+        this.scriptForm = scriptForm;
+        this.player = player;
+    }
+
+    @Override
+    public Player getPlayer() {
+        return player;
+    }
+
+    public ScriptForm getScriptForm() {
+        return scriptForm;
+    }
+}
